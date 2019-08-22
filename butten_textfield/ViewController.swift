6a04c7e8c4e2b4d61b5544c4740611e2a2d42bc6
COE-030 <coe-030@coe-030s-imac.local>
2019-08-22 15:43:04 +0530

    it is my first UI part

--- butten_textfield/ViewController.swift
+++ butten_textfield/ViewController.swift
@@ -9,7 +9,37 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    
+    @IBOutlet weak var txtEmailid: UITextField!
+    
+    
+    
+    @IBOutlet weak var txtPassword: UITextField!
+    
+    @IBOutlet weak var txtConformpassword: UITextField!
+    
+    
+    @IBOutlet weak var lblMessage: UILabel!
+    
+    @IBAction func setPasswordVisibilitychange(_ sender: Any)
+    {
+        txtPassword.isSecureTextEntry = !( txtPassword.isSecureTextEntry)
+        txtConformpassword.isSecureTextEntry = !( txtConformpassword.isSecureTextEntry)
+    }
+    @IBAction func btnSignupTouchup(_ sender: Any)
+    {
+    let emailid = txtEmailid.text
+    let password = txtPassword.text
+    let cornform = txtConformpassword.text
+    
+        if password != (cornform)
+         {
+            lblMessage.text = "Conform password is not match"
+        }
+        
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
